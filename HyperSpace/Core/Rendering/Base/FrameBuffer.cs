842ca44076b694dc5419b65b5c50e18c2b5ff944
macbury <macbury@gmail.com>
2013-12-02 10:06:55 +0100

    Refactoring

---
+++ HyperSpace/Core/Rendering/Base/FrameBuffer.cs
@@ -0,0 +1,33 @@
+﻿using HyperSpace.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperSpace.Core.Rendering.Base {
+  class FrameBuffer : Disposable {
+    private int frameBufferHandle;
+    private int depthBufferHandle;
+    private int _width, _height;
+    private Texture colorTexture;
+    private bool hasDepth;
+    private bool isDirty;
+
+    public FrameBuffer(int w, int h, bool hasDepth) {
+      this.hasDepth = hasDepth;
+      this._width   = w;
+      this._height  = h;
+      build();
+      Game.glResources.Add(this);
+    }
+
+    private void build() {
+      colorTexture = new Texture();
+    }
+
+    public void dispose() {
+      Game.glResources.Remove(this);
+    }
+  }
+}
---
+++ HyperSpace/Core/Scenes/Tests/FrameBufferTest.cs
@@ -0,0 +1,36 @@
+﻿using HyperSpace.Core.Rendering.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperSpace.Core.Scenes.Tests {
+  class FrameBufferTest : Scene{
+    private FrameBuffer fbo;
+
+    public void onEnter() {
+      this.fbo = new FrameBuffer();
+    }
+
+    public void resize() {
+      throw new NotImplementedException();
+    }
+
+    public void update(double delta) {
+      throw new NotImplementedException();
+    }
+
+    public void render() {
+      throw new NotImplementedException();
+    }
+
+    public void onExit() {
+      throw new NotImplementedException();
+    }
+
+    public void dispose() {
+      this.fbo.dispose();
+    }
+  }
+}
